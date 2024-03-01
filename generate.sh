bd3aa6bb3bd771c789a1ccda91e7ddbd84be4be3
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2024-03-01 03:06:15 -0500

    update

--- generate.sh
+++ generate.sh
@@ -9,11 +9,5 @@ cp temp/output/littlelink-generator .
 # Run the littlelink-generator project
 ./littlelink-generator --asset-path "assets/" --config "links.json"
 
-# Check if the build directory exists in the temporary directory
-if [ -d "temp/build" ]; then
-    # Copy the build directory to the root directory
-    cp -r temp/build /build
-fi
-
 # Clean up temporary directory
 rm -rf temp
